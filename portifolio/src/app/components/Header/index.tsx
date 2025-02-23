8df0d45e9ab199ebafa82e8e3cf6fa9112103449
Joao-Victor-Martins <joaovictorferreiramartins02@gmail.com>
2025-02-23 15:53:22 -0300

    Algumas atualizações do Header feitas, porem ainda falta concluir algumas revisões

---
+++ portifolio/src/app/components/Header/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+import styles from './Header.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function Header() {
+
+    const [theme, setTheme] = useState("light");
+
+    const toggleThemer = () => {
+        theme === 'light' ? setTheme('dark') : setTheme('light');
+    }
+
+    return (
+        <header className={theme === "light" ? "bg-cyberTech-hard_blue flex justify-center" : "dark:bg-black"}>
+            <h1 className='text-center'>Olá Mundo</h1>
+            <button className='bg-cyberTech-soft_blue text-cyberTech-hard_blue' onClick={toggleThemer}>
+                {theme === 'light' ? "Mudar para Dark" : "Mudar para Light"}
+            </button>
+        </header>
+    )
+}
